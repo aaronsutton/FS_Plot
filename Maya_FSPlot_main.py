7642c44e2b763081590e578aa71a5581692e53f0
Aaron Sutton <aaron@asutton.net>
2014-05-15 13:56:53 -0400

    Changed translation

--- Maya_FSPlot_main.py
+++ Maya_FSPlot_main.py
@@ -1,46 +1,37 @@
-#import re
 import numpy as np
 import matplotlib.pyplot as plt
 import itertools
-#import matplotlib.figure as fig
+from generateNN import genNN
+# import matplotlib.figure as fig
 from mpl_toolkits.mplot3d import Axes3D
 from mayavi import mlab
 # from tvtk.api import tvtk, write_data
 # from scipy.interpolate import griddata
 from pyhull.voronoi import VoronoiTess
 from return_data import return_data
-#import gts
-#from pyhull.delaunay import DelaunayTri
-#import mayavi
-#import pyvtk
+# import gts
+# from pyhull.delaunay import DelaunayTri
+# import mayavi
+# import pyvtk
 
 assert plt
 assert Axes3D
 assert itertools
 # assert tvtk
-#assert pyvtk
+# assert pyvtk
 
 filename = 'YbSO25072013.bxsf.band-39'
-#Try maya plotting stuff
-#f_plot = open('data_' + filename + '.dat', 'r')
-#plotData = np.loadtxt(f_plot)
-#plotX = plotData[:, 0]
-#plotY = plotData[:, 1]
-#plotZ = plotData[:, 2]
-#Energy = plotData[:, 3]
-#R = plotData[:, 0:3]
-#pts = 30j
-#X, Y, Z = np.mgrid[min(plotX):max(plotX):pts, min(plotY):max(plotY):pts, min(plotZ):max(plotZ):pts]
-#F = griddata(R, Energy, (X, Y, Z), method='linear')
-#f_plot.close()
 X, Y, Z, F = return_data(filename)
+
+write_NN = 0
+E_F, G_1, G_2, G_3 = genNN(filename, write_NN)
 
 NN_plot = open('NNgen.dat', 'r')
 points = np.loadtxt(NN_plot)
-#NN_plot.close()
+# NN_plot.close()
 pointsarray = np.array(points)
-#points3d(pointsarray[:, 0], pointsarray[:, 1], pointsarray[:, 2], color=(1, 0, 0))
-#points3d(0, 0, 0, color=(1, 0, 0))
+# points3d(pointsarray[:, 0], pointsarray[:, 1], pointsarray[:, 2], color=(1, 0, 0))
+# points3d(0, 0, 0, color=(1, 0, 0))
 d = VoronoiTess(points)
 dpoints = d.points
 ridges = d.ridges.items()
@@ -51,9 +42,8 @@ bzfinalx = list()
 bzfinaly = list()
 bzfinalz = list()
 bzlines = open('bzlines.dat', 'w')
-#mayawindow = mayavi.mayavi()i
 
-mlab.figure(1, bgcolor=(1,1,1), size=(325, 325))
+mlab.figure(1, bgcolor=(1, 1, 1), size=(325, 325))
 
 for (p1, p2), vertind in ridges:
     if p1 == 0:
@@ -64,7 +54,7 @@ for (p1, p2), vertind in ridges:
             bzplotx.append(vertices[aind][0])
             bzploty.append(vertices[aind][1])
             bzplotz.append(vertices[aind][2])
-        #This was used to create closed faces for the outline of the BZ, not needed if creating a Delaunay 3D surface
+        # This was used to create closed faces for the outline of the BZ, not needed if creating a Delaunay 3D surface
         if bzplotx[0] != bzplotx[-1] or bzploty[0] != bzploty[-1] or bzplotz[0] != bzplotz[-1]:
             bzplotx.append(bzplotx[0])
             bzploty.append(bzploty[0])
@@ -78,52 +68,60 @@ for (p1, p2), vertind in ridges:
         '''for i in range(len(bzplotxa)):
             outline = "{}\t{}\t{}\n".format(bzplotxa[i], bzplotya[i], bzplotza[i])
             bzlines.write(outline)'''
-        #Plot the outline
+        # Plot the outline
         mlab.plot3d(bzplotxa, bzplotya, bzplotza, color=(0, 0, 0), line_width=0.001, tube_radius=0.001)
 bzlines.close()
 scalefactor = max(bzfinalz)/Z.max()
 Xnew = X.copy()
 Ynew = Y.copy()
 Znew = Z.copy()
-#print Znew.max()
-#contour3d(X, Y, Z + 2*max(plotZ), F)
+# print Znew.max()
+# contour3d(X, Y, Z + 2*max(plotZ), F)
 
 bzfile = open('bzpoints.dat', 'w')
 for i in range(len(bzfinalx)):
     outstring = "{}\t{}\t{}\n".format(bzfinalx[i], bzfinaly[i], bzfinalz[i])
     bzfile.write(outstring)
 bzfile.close()
-FS = mlab.contour3d(Xnew, Ynew, Znew, F, contours=[0.765467])
+
+E_F_shift = 0.018
+translate = [1, 0, 1]
+shift = translate[0] * G_1 + translate[1] * G_2 + translate[2] * G_3
+
+
+FS = mlab.contour3d(Xnew, Ynew, Znew, F, contours=[E_F + E_F_shift])
 bzpoints = mlab.points3d(bzfinalx, bzfinaly, bzfinalz, color=(0, 0, 0), scale_factor=0.001)
 
 # Plot a second band?
 z_shift = 1
 if z_shift:
-    z_shift_val = Z.max() - Z.min()
-    Z_z = [x + z_shift_val for x in Z]
-    FS_Z = mlab.contour3d(X, Y, Z_z, F, contours=[0.765467])
-    xz_shift_val = 2*X.max()
-    z_shift_val = Z.max()
-    X_xz = [x + xz_shift_val - 0.02 for x in X]
-    Z_xz = [x + z_shift_val for x in Z]
-    FS_XZ = mlab.contour3d(X_xz, Y, Z_xz, F, contours=[0.765467])
+    X_shift = [x + shift[0] for x in X]
+    Y_shift = [y + shift[1] for y in Y]
+    Z_shift = [z + shift[2] for z in Z]
+    FS_shift = mlab.contour3d(X_shift, Y_shift, Z_shift, F, contours=[E_F + E_F_shift])
+
+  #  z_shift_val = Z.max() - Z.min()
+  #  Z_z = [x + z_shift_val for x in Z]
+  #  FS_Z = mlab.contour3d(X, Y, Z_z, F, contours=[E_F + E_F_shift])
+  #  xz_shift_val = 2*X.max()
+  #  z_shift_val = Z.max()
+  #  X_xz = [x + xz_shift_val - 0.02 for x in X]
+  #  Z_xz = [x + z_shift_val for x in Z]
+  #  FS_XZ = mlab.contour3d(X_xz, Y, Z_xz, F, contours=[E_F + E_F_shift])
 
 plot_band2 = 0
 filename2 = 'YbSO25072013.bxsf.band-40'
 
 if plot_band2:
     X, Y, Z, F = return_data(filename2)
-    FS2 = mlab.contour3d(X, Y, Z, F, contours=[0.765467])
+    FS2 = mlab.contour3d(X, Y, Z, F, contours=[E_F + E_F_shift])
 
 # Save plots - various options
-#mlab.savefig(filename='test.wrl')
-#savefig('FS' + filename + '.png')
-#delmesh = mlab.pipeline.delaunay3d(bzpoints)
-#bzpoints.remove
-#delsurface = mlab.pipeline.surface(delmesh, opacity=0.5)
-
-
-
+# mlab.savefig(filename='test.wrl')
+# savefig('FS' + filename + '.png')
+# delmesh = mlab.pipeline.delaunay3d(bzpoints)
+# bzpoints.remove
+# delsurface = mlab.pipeline.surface(delmesh, opacity=0.5)
 
 # This was a test of using the gts package. It sort of works, but isn't necessary
 '''isotest = gts.isosurface(F,0.765467)
@@ -148,9 +146,6 @@ for o in outerlist:
     for p in o:
         facesinner.append(p)
     print facesinner'''
-
-
-
 
 # clip = mlab.pipeline.data_set_clipper(FS)
 # transform = tvtk.Transform()
--- generateNN.py
+++ generateNN.py
@@ -1,34 +1,36 @@
 import numpy as np
 
-with open('/home/asutton/Dropbox/WIEN2K/CASES/YbSO25072013/YbSO25072013.bxsf.band-39', 'r') as f_id:
-    f_id.seek(0)
-    line = f_id.readline()
-    line = f_id.readline()
-    E_F = float(line.split()[2])
-    line = f_id.readline()
-    line = f_id.readline()
-    line = f_id.readline()
-    line = f_id.readline()
-    line = f_id.readline()
-    no_bands = int(line.split()[0])
-    line = f_id.readline()
-    dimensions = int(line.split()[0]), int(line.split()[1]), int(line.split()[2])
-    line = f_id.readline()
-    line = f_id.readline()
-    G_1 = float(line.split()[0]), float(line.split()[1]), float(line.split()[2])
-    line = f_id.readline()
-    G_2 = float(line.split()[0]), float(line.split()[1]), float(line.split()[2])
-    line = f_id.readline()
-    G_3 = float(line.split()[0]), float(line.split()[1]), float(line.split()[2])
+def genNN(filename, write_NN):
+    with open(filename, 'r') as f_id:
+        f_id.seek(0)
+        line = f_id.readline()
+        line = f_id.readline()
+        E_F = float(line.split()[2])
+        line = f_id.readline()
+        line = f_id.readline()
+        line = f_id.readline()
+        line = f_id.readline()
+        line = f_id.readline()
+        no_bands = int(line.split()[0])
+        line = f_id.readline()
+        dimensions = int(line.split()[0]), int(line.split()[1]), int(line.split()[2])
+        line = f_id.readline()
+        line = f_id.readline()
+        G_1 = float(line.split()[0]), float(line.split()[1]), float(line.split()[2])
+        line = f_id.readline()
+        G_2 = float(line.split()[0]), float(line.split()[1]), float(line.split()[2])
+        line = f_id.readline()
+        G_3 = float(line.split()[0]), float(line.split()[1]), float(line.split()[2])
 
-
-f = open('NNgen.dat', 'w')
-i, j, k = 0, 0, 0
-plusminusone = [0, -1, 1]
-for i in plusminusone:
-            for j in plusminusone:
-                for k in plusminusone:
-                    NN = i*np.array(G_1) + j*np.array(G_2) + k*np.array(G_3)
-                    outstr = "{}\t{}\t{}\n".format(NN[0], NN[1], NN[2])
-                    f.write(outstr)
-f.close()
+    if write_NN:
+        f = open('NNgen.dat', 'w')
+        i, j, k = 0, 0, 0
+        plusminusone = [0, -1, 1]
+        for i in plusminusone:
+                    for j in plusminusone:
+                        for k in plusminusone:
+                            NN = i*np.array(G_1) + j*np.array(G_2) + k*np.array(G_3)
+                            outstr = "{}\t{}\t{}\n".format(NN[0], NN[1], NN[2])
+                            f.write(outstr)
+        f.close()
+    return E_F, G_1, G_2, G_3
